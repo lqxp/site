0c28060441b2991c2a6d48bd6aa76cc1729e83e9
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2026-06-29 14:01:21 +0200

    fixes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,11 +16,10 @@ export default defineNuxtConfig({
         { name: "theme-color", content: "#07111f" },
       ],
       link: [
-        {
-          rel: "icon",
-          type: "image/svg+xml",
-          href: "/app-icon.svg",
-        },
+        { rel: "icon", href: "/favicon.ico", sizes: "any" },
+        { rel: "icon", type: "image/svg+xml", href: "/app-icon.svg" },
+        { rel: "icon", type: "image/png", sizes: "128x128", href: "/128x128.png" },
+        { rel: "apple-touch-icon", sizes: "128x128", href: "/128x128.png" },
       ],
     },
   },
